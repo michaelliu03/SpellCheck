5e838fc06571ad5a1d1e9a6affb07f8a099ecea4
michaelliu <841412988@qq.com>
2018-07-04 14:19:42 +0800

    trie node

--- src/main/java/com/vip/nlp/TrieNode.java
+++ src/main/java/com/vip/nlp/TrieNode.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * @author michael.liu
  * @Title: TrieNode
  * @ProjectName SpellCheck
- * @Description: TODO
+ * @Description: trie node
  * @date 2018/7/414:10
  */
 
@@ -17,4 +17,41 @@ public class TrieNode<T> {
     private T nodeValue;
     private boolean terminal;
     private Map<Character, TrieNode<T>> children = new HashMap<Character, TrieNode<T>>();
+
+    public Character getNodeKey() {
+        return nodeKey;
+    }
+
+    public void setNodeKey(Character nodeKey) {
+        this.nodeKey = nodeKey;
+    }
+
+    public T getNodeValue() {
+        return nodeValue;
+    }
+
+    public void setNodeValue(T nodeValue) {
+        this.nodeValue = nodeValue;
+    }
+
+    public boolean isTerminal() {
+        return terminal;
+    }
+
+    public void setTerminal(boolean terminal) {
+        this.terminal = terminal;
+    }
+
+    public Map<Character, TrieNode<T>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Map<Character, TrieNode<T>> children) {
+        this.children = children;
+    }
+
+    public String toString()
+    {
+        return String.valueOf(nodeKey);
+    }
 }
