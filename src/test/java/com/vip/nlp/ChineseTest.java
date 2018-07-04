088d8294795b01205b4eed16d4478b5c41445ddd
michaelliu <841412988@qq.com>
2018-07-04 16:03:58 +0800

    filter testcase

--- src/test/java/com/vip/nlp/ChineseTest.java
+++ src/test/java/com/vip/nlp/ChineseTest.java
@@ -2,6 +2,12 @@ package com.vip.nlp;
 
 import junit.framework.TestCase;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * @author michael.liu
  * @Title: ChineseTest
@@ -13,7 +19,21 @@ import junit.framework.TestCase;
 
 public class ChineseTest extends TestCase {
 
+    public final static String  InputFilePath = "./data/mingci.csv";
     public void loadData(){
+    try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(InputFilePath)));
+            String line = "";
 
+            while ((line = br.readLine()) != null) {
+                String sentence = line.trim();
+
+
+            }
+
+            br.close();
+        } catch (IOException e) {
+            e.getMessage();
+        }
     }
 }
